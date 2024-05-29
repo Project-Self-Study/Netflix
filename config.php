b247b7d59c8ffcc3370aba32794f7c4e1679f550
Ntokozo254 <141838004+Ntokozo254@users.noreply.github.com>
2024-05-29 13:36:18 +0200

    Update config.php
    Just changed database name

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'wheatley.cs.up.ac.za');
 define('DB_USERNAME', 'u22506773');
 define('DB_PASSWORD', 'MAIUBAXLVHG5R7XOPBG2RAU65YCINHDB');  // Use an appropriate password if set
-define('DB_DATABASE', 'u22506773_Area221');
+define('DB_DATABASE', 'u22506773_dummy');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
