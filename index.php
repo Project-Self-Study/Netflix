cc0c71f02e1a116c4d4a6509061f910b3bbeece2
Ntokozo254 <u22506773@tuks.co.za>
2024-05-29 22:17:37 +0200

    new file:   donkey.jpg 	modified:   index.css 	modified:   index.php

--- index.php
+++ index.php
@@ -4,71 +4,188 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href ="index.css"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-        <title>Document</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div id="container"></div>
+    
+    <h1 class="header">Stream.Binge.Repeat.Hoop has it all!</h1>
 
-    <body>
-        <div class="container">
-        </div>
-      
-        <h1 class = "header">Stream.Binge.Repeat.Hoop has it all!</h1>
-
-        <h2 class = "category-title">Category1</h2>
-        <div class = "category">
-            <div class="show">
+    <h2 class="category-title">Category1</h2>
+    <div class="category">
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
             </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
             </div>
         </div>
-
-
-        <h2 class = "category-title">Category2</h2>
-        <div class = "category">
-            <div class="show">
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
             </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
-            </div>
-            <div class="show">
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
             </div>
         </div>
-    </body>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
 
+    </div>
 
+    <h2 class="category-title">Category2</h2>
+    <div class="category">
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+        <div class="show">
+            <div class="show-title">
+                <h3>Donkey Man</h3>
+            </div>
+            <div class="show-image-container">
+                <img class="show-image" src="donkey.jpg" alt="show image">
+            </div>
+        </div>
+
+    </div>
+</body>
 </html>
